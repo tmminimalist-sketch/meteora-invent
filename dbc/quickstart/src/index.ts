602a7b5076c1d74ad5095e2d97459e7a269b4364
Joshua <joshua@raccoons.dev>
2025-05-25 23:03:26 +0800

    changed quickstart to use config.ts

--- dbc/quickstart/src/index.ts
+++ dbc/quickstart/src/index.ts
@@ -1,13 +1,6 @@
 import {
-  ActivationType,
   buildCurveWithMarketCap,
-  CollectFeeMode,
   DynamicBondingCurveClient,
-  FeeSchedulerMode,
-  MigrationFeeOption,
-  MigrationOption,
-  TokenDecimal,
-  TokenType,
 } from "@meteora-ag/dynamic-bonding-curve-sdk";
 import bs58 from "bs58";
 import {
@@ -16,8 +9,10 @@ import {
   sendAndConfirmTransaction,
 } from "@solana/web3.js";
 import "dotenv/config";
-import { NATIVE_MINT } from "@solana/spl-token";
+import { quoteMint, configKeyParams, tokenParams } from "../config";
 
+
+console.log(configKeyParams);
 const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY;
 if (!WALLET_PRIVATE_KEY) {
   throw new Error("PRIVATE_KEY is not set");
@@ -35,48 +30,16 @@ async function main() {
   // Step 1: Create Config Key
   const configKey = Keypair.generate();
 
-  const curveConfig = buildCurveWithMarketCap({
-    totalTokenSupply: 1000000000,
-    initialMarketCap: 100,
-    migrationMarketCap: 100.1,
-    migrationOption: MigrationOption.MET_DAMM_V2,
-    tokenBaseDecimal: TokenDecimal.SIX,
-    tokenQuoteDecimal: TokenDecimal.NINE,
-    lockedVestingParam: {
-      totalLockedVestingAmount: 10000000,
-      numberOfVestingPeriod: 365,
-      cliffUnlockAmount: 0,
-      totalVestingDuration: ((365 * 24 * 60 * 60) / 1000) * 400,
-      cliffDurationFromMigrationTime: 0,
-    },
-    feeSchedulerParam: {
-      startingFeeBps: 5000,
-      endingFeeBps: 100,
-      numberOfPeriod: 100,
-      totalDuration: 10 * 60 * 60,
-      feeSchedulerMode: FeeSchedulerMode.Exponential,
-    },
-    dynamicFeeEnabled: true,
-    activationType: ActivationType.Slot,
-    collectFeeMode: CollectFeeMode.OnlyQuote,
-    migrationFeeOption: MigrationFeeOption.FixedBps100,
-    tokenType: TokenType.SPL,
-    partnerLpPercentage: 0,
-    creatorLpPercentage: 0,
-    partnerLockedLpPercentage: 50,
-    creatorLockedLpPercentage: 50,
-    creatorTradingFeePercentage: 50,
-    leftover: 0,
-  });
+    const curveConfig = buildCurveWithMarketCap(configKeyParams);
 
   console.log(curveConfig);
 
   const createConfigTx = await client.partner.createConfig({
     config: configKey.publicKey,
-    quoteMint: NATIVE_MINT,
     feeClaimer: wallet.publicKey,
     leftoverReceiver: wallet.publicKey,
     payer: wallet.publicKey,
+    quoteMint: quoteMint,
     ...curveConfig,
   });
 
@@ -103,9 +66,7 @@ async function main() {
   const baseMint = Keypair.generate();
 
   const createPoolTx = await client.pool.createPool({
-    name: "Moon Token",
-    symbol: "MOON",
-    uri: "https://i.pinimg.com/736x/9b/80/f6/9b80f613d125c9efd816d0be243aa1c0.jpg",
+    ...tokenParams,
     config: configKey.publicKey,
     baseMint: baseMint.publicKey,
     payer: wallet.publicKey,
